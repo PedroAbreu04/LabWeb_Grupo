9df44e3625cdba28e6486bc8a37bb672183165c2
JoaoLimaV <joaolima675@outlook.com>
2023-09-26 14:43:44 -0300

    Correção de Css CardsProduct, Ativação de Validação de Login e Page NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,30 @@
 function NotAuthenticated() {
     return (
-        <h1> NotFound </h1>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+
+                width: '100%',
+                height: '100%'
+            }}
+        >
+            <h1> NotFound </h1>
+
+            <a
+                style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    marginTop: '10px'
+                }}
+                href="/login"
+            >
+                Voltar para Login
+            </a>
+
+        </div>
     );
 }
 
